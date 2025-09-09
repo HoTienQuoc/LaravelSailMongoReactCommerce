d770110f1a19d6875e66a2d92fcd418e06fe178f
HoTienQuoc <hotienquoc0429@gmail.com>
2025-09-09 23:01:51 +0700

    Refactor admin routes to use AdminMiddleware, update sidebar layout to comment out unused links, and enhance application bootstrap with middleware aliasing.

--- backend/resources/views/admin/layouts/sidebar.blade.php
+++ backend/resources/views/admin/layouts/sidebar.blade.php
@@ -12,7 +12,7 @@
                         Dashboard
                     </a>
                 </li>
-                <li class="nav-item">
+                {{-- <li class="nav-item">
                     <a class="nav-link d-flex align-items-center gap-2" aria-current="page" href="{{route('admin.colors.index')}}">
                         <i class="fas fa-palette"></i>
                         Colors
@@ -53,7 +53,7 @@
                         <i class="fas fa-users"></i>
                         Users
                     </a>
-                </li>
+                </li> --}}
             </ul>
             <hr class="my-3">
             <ul class="nav flex-column mb-auto">
